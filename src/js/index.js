249f9697076615fd439f11457ebf9b272e827632
yumna65 <71685041+yumna65@users.noreply.github.com>
2021-04-13 23:44:25 -0400

    L

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
-/* import "../js/question"; */
-
-import { questions } from "./questions";
 
 console.log("connected");
 
-function startQuiz() {}
+const startButton = document.getElementById('btn-before')
+startButton.addEventListener('click', startGame)
+
+function startQuiz() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion()
