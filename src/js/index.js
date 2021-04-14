dcdf4099ff097f5d76166a1e7a77f45d9cc3449a
stanleyhuang1 <stanley.huang22@sitechhs.com>
2021-04-14 00:00:11 -0400

    yay

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,79 @@
-
 console.log("connected");
 
-const startButton = document.getElementById('btn-before')
-startButton.addEventListener('click', startGame)
+const questions = [
+  {
+    question: "Have you talked to Mr. Erlenwein?",
+    answers: [
+      { text: "yes", correct: true },
+      { text: "no", correct: false },
+    ],
+  },
+  {
+    question: "Rate your personality",
+    answers: [
+      { text: "Mediocre", correct: false },
+      { text: "Outstanding", correct: true },
+      { text: "Horrible", correct: false },
+      { text: "Pretty Good", correct: true },
+    ],
+  },
+  {
+    question: "What class do you attend or teach (Choose your favorite)",
+    answers: [
+      { text: "AP Computer Science", correct: false },
+      { text: "AP Psychology", correct: true },
+      { text: "English", correct: true },
+      { text: "AP Calc BC", correct: false },
+    ],
+  },
+  {
+    question: "How many AP Classes did you take or teach?",
+    answers: [
+      { text: "4", correct: false },
+      { text: "5", correct: true },
+      { text: "6", correct: true },
+      { text: "7(+)", correct: false },
+    ],
+  },
+  {
+    question: "Do you coach a sports team or are you on a sports team",
+    answers: [
+      { text: "yes", correct: true },
+      { text: "no", correct: false },
+      { text: "I am planning on joining", correct: true },
+      { text: "I am in clubs", correct: false },
+    ],
+  },
+  {
+    question: "Bonus: Are you Mr. Whalen?",
+    answers: [
+      { text: "yes", correct: true },
+      { text: "no", correct: false },
+    ],
+  },
+];
+
+const startButton = document.getElementById("btn-before");
+console.log(startButton);
+
+const questionContainerElement = document.getElementById("quiz-questions");
+console.log(questionContainerElement);
+
+const questionAsked = questions.question;
+console.log(questionAsked);
+
+const answerBtn = document.getElementById("questions.answer");
+
+const startButton = document.getElementById("btn-before");
+startButton.addEventListener("click", startGame);
 
 function startQuiz() {
-    console.log('started')
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+  console.log("started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+  // setNextQuestion();
 }
 
-function setNextQuestion()
+function setNextQuestion() {
+  revealQuestion(questions);
+}
