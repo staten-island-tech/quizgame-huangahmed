b358a6ff69a5f5202d6e9b9ea884c8b245d85ccd
stanleyhuang1 <stanley.huang22@sitechhs.com>
2021-04-13 22:05:31 -0400

    s

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
-import "../js/question";
 
 console.log("connected");
 
-function startQuiz() {}
+const startButton = document.getElementById('btn-before')
+startButton.addEventListener('click', startGame)
+
+function startQuiz() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion()
